de41240dc3dcd831703aacc0b08528d2c8c30f21
Julia Pak <jpak0444@gmail.com>
2022-10-06 16:52:43 -0400

    spl

--- lib/pages/splash_pg.dart
+++ lib/pages/splash_pg.dart
@@ -1,26 +1,21 @@
+import 'package:coffeesoc/colors/colours_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gradients_reborn/flutter_gradients_reborn.dart';
 import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          gradient: FlutterGradients.confidentCloud(
-        type: GradientType.linear,
-        tileMode: TileMode.clamp,
-      )),
-      child: Center(
+    return Scaffold(
+      backgroundColor: containerback,
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           // mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Lottie.network(
-                'https://assets1.lottiefiles.com/private_files/lf30_y4Fapb.json'),
+                'https://assets6.lottiefiles.com/private_files/lf30_y4Fapb.json')
           ],
         ),
       ),
