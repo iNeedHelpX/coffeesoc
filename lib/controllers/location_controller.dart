64a025006facbde3f46696804f3aa94bbc2d845d
Julia Pak <jpak0444@gmail.com>
2022-10-13 21:42:23 -0400

    location

---
+++ lib/controllers/location_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+//controller for location
+class LocationController extends GetxController {
+  static LocationController instance = Get.find();
+}
--- lib/globalvars.dart
+++ lib/globalvars.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:coffeesoc/controllers/location_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:coffeesoc/controllers/login_controller.dart';
@@ -11,6 +12,6 @@ FirebaseAuth auth = FirebaseAuth.instance;
 
 //controller
 LoginController loginController = LoginController.instance;
-
+LocationController locationController = LocationController.instance;
 //other variables
 Logger logger = Logger();
