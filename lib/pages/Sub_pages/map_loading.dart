8ed09972e6466525cc06abf62ab8344bd1d69323
Julia Pak <jpak0444@gmail.com>
2022-10-14 08:18:21 -0400

    cp

--- lib/pages/Sub_pages/map_loading.dart
+++ lib/pages/Sub_pages/map_loading.dart
@@ -2,20 +2,35 @@ import 'package:coffeesoc/colors/colours_list.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoadScreen extends StatelessWidget {
+class LoadScreen extends StatefulWidget {
   const LoadScreen({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<LoadScreen> createState() => _LoadScreenState();
+}
+
+class _LoadScreenState extends State<LoadScreen> {
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text(
-          'Loading map, please wait..',
-          style: GoogleFonts.dancingScript(
-              fontSize: 26, fontWeight: FontWeight.w500, color: brightpink),
-        ),
+      child: Stack(
+        children: [
+          Center(
+            child: Text(
+              'Loading map, please wait..',
+              style: GoogleFonts.dancingScript(
+                  fontSize: 26, fontWeight: FontWeight.w500, color: brightpink),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          CircularProgressIndicator(
+            color: tabicon,
+          )
+        ],
       ),
     );
   }
