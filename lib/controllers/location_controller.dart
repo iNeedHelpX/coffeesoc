02d572f5c9748f79c3eafa9ab7b07a80ddef935c
Julia Pak <jpak0444@gmail.com>
2022-10-13 23:04:15 -0400

    pos

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -27,5 +27,24 @@ class LocationController extends GetxController {
   getlocation() async {
     bool serviceEnabled;
     LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Geolocator.openLocationSettings();
+      return Future.error("location service is not enabled");
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      //do stuff here
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        //stuff
+        return Future.error("location permissions denied");
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error("location permissions permanently denied");
+    }
+    streamSubscription =
+        Geolocator.getPositionStream().listen((Position position) {});
   }
 }
