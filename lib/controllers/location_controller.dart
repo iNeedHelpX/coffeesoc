208394331a8b100c0633b9a09baef0ee5a94980f
Julia Pak <jpak0444@gmail.com>
2022-10-13 22:19:06 -0400

    locatiom

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -4,6 +4,10 @@ import 'package:get/get.dart';
 class LocationController extends GetxController {
   static LocationController instance = Get.find();
 
+  var lat = "getting latitude..".obs;
+  var lon = "getting longitude..".obs;
+  var address = "getting address..".obs;
+
   @override
   void onReady() {
     super.onReady();
