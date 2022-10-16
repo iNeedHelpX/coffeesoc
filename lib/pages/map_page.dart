82fbf44cc0c46d2d8e7ffa96a2a871fd713b31e2
Julia Pak <jpak0444@gmail.com>
2022-10-15 23:47:02 -0400

    terinary op

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:coffeesoc/globalvars.dart';
+import 'package:coffeesoc/pages/Sub_pages/map_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -16,33 +17,35 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // you must wrap the map within a container or else it will say something about render box not being laid out and this: Another exception was thrown: RenderUiKitView object was given an infinite size during layout.
+      body: locationController.myLocation == false
+          ? LoadScreen()
+          : Stack(
+              children: [
+                // you must wrap the map within a container or else it will say something about render box not being laid out and this: Another exception was thrown: RenderUiKitView object was given an infinite size during layout.
 
-          Container(
-            child: GoogleMap(
-              myLocationEnabled: true,
-              myLocationButtonEnabled: false,
-              initialCameraPosition: CameraPosition(
-                  //get user location
-                  target: LatLng(locationController.myLocation!.latitude,
-                      locationController.myLocation!.longitude),
-                  zoom: 16),
-              minMaxZoomPreference: MinMaxZoomPreference(15.5, 19),
-              zoomGesturesEnabled: true,
+                Container(
+                  child: GoogleMap(
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: false,
+                    initialCameraPosition: CameraPosition(
+                        //get user location
+                        target: LatLng(locationController.myLocation!.latitude,
+                            locationController.myLocation!.longitude),
+                        zoom: 16),
+                    minMaxZoomPreference: MinMaxZoomPreference(15.5, 19),
+                    zoomGesturesEnabled: true,
 
-              //this sets the scroll limit so that there is no excessive API usage from scroll off
-              cameraTargetBounds: CameraTargetBounds(
-                LatLngBounds(
-                  northeast: LatLng(43.7970928, -79.3067414),
-                  southwest: LatLng(43.592580, -79.483674),
+                    //this sets the scroll limit so that there is no excessive API usage from scroll off
+                    cameraTargetBounds: CameraTargetBounds(
+                      LatLngBounds(
+                        northeast: LatLng(43.7970928, -79.3067414),
+                        southwest: LatLng(43.592580, -79.483674),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ),
-        ],
-      ),
     );
   }
 }
