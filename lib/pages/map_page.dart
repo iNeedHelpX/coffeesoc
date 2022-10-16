7e69bfdec912b2b01eb20c95feb8391bf6fbce89
Julia Pak <jpak0444@gmail.com>
2022-10-16 16:08:17 -0400

    ok

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -18,35 +18,33 @@ class _MapPageState extends State<MapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       //this here doesn't work?
-      body: locationController.myLocation == false
-          ? LoadScreen()
-          : Stack(
-              children: [
-                // you must wrap the map within a container or else it will say something about render box not being laid out and this: Another exception was thrown: RenderUiKitView object was given an infinite size during layout.
+      body: Stack(
+        children: [
+          // you must wrap the map within a container or else it will say something about render box not being laid out and this: Another exception was thrown: RenderUiKitView object was given an infinite size during layout.
 
-                Container(
-                  child: GoogleMap(
-                    myLocationEnabled: true,
-                    myLocationButtonEnabled: false,
-                    initialCameraPosition: CameraPosition(
-                        //get user location
-                        target: LatLng(locationController.myLocation!.latitude,
-                            locationController.myLocation!.longitude),
-                        zoom: 16),
-                    minMaxZoomPreference: MinMaxZoomPreference(15.5, 19),
-                    zoomGesturesEnabled: true,
+          Container(
+            child: GoogleMap(
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              initialCameraPosition: CameraPosition(
+                  //get user location
+                  target: LatLng(locationController.myLocation!.latitude,
+                      locationController.myLocation!.longitude),
+                  zoom: 16),
+              minMaxZoomPreference: MinMaxZoomPreference(15.5, 19),
+              zoomGesturesEnabled: true,
 
-                    //this sets the scroll limit so that there is no excessive API usage from scroll off
-                    cameraTargetBounds: CameraTargetBounds(
-                      LatLngBounds(
-                        northeast: LatLng(43.7970928, -79.3067414),
-                        southwest: LatLng(43.592580, -79.483674),
-                      ),
-                    ),
-                  ),
+              //this sets the scroll limit so that there is no excessive API usage from scroll off
+              cameraTargetBounds: CameraTargetBounds(
+                LatLngBounds(
+                  northeast: LatLng(43.7970928, -79.3067414),
+                  southwest: LatLng(43.592580, -79.483674),
                 ),
-              ],
+              ),
             ),
+          ),
+        ],
+      ),
     );
   }
 }
