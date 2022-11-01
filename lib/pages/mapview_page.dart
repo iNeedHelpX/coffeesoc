373b0eaa1c2dd792e22f05c1c38d00e499805e5a
Julia Pak <jpak0444@gmail.com>
2022-11-01 19:36:20 -0400

    map view pg

---
+++ lib/pages/mapview_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MapsViewPg extends StatefulWidget {
+  const MapsViewPg({super.key});
+
+  @override
+  State<MapsViewPg> createState() => _MapsViewPgState();
+}
+
+class _MapsViewPgState extends State<MapsViewPg> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
