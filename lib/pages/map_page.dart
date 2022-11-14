847085b4041c35db902bd94f8aa6f21caf87ecde
Julia Pak <jpak0444@gmail.com>
2022-11-14 03:47:42 -0500

    change to icon

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,6 +1,9 @@
+import 'package:coffeesoc/colors/colours_list.dart';
+import 'package:coffeesoc/colors/greygradient.dart';
 import 'package:coffeesoc/controllers/location_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 //this is the page that shows the google map
@@ -13,6 +16,74 @@ class MapPage extends StatelessWidget {
     final MediaQueryData mediaQuery = MediaQuery.of(context);
     final LocationController locController =
         Get.put<LocationController>(LocationController());
+
+    Widget buildInfo() {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            height: mediaQuery.size.height * 0.20,
+            width: mediaQuery.size.width,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: mediaQuery.size.width * 0.05,
+                vertical: mediaQuery.size.height * 0.01,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Select Location",
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.poppins(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                      color: tabicon,
+                    ),
+                  ),
+                  Text(
+                    'Drag to select the location of your recommendation',
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.poppins(
+                      fontSize: 14,
+                      color: bg,
+                    ),
+                  ),
+                  SizedBox(height: mediaQuery.size.height * 0.02),
+                  Center(
+                    child: RoundedButton(
+                      buttonText: "Search",
+                      iconWithSizedBox: Row(
+                        children: [
+                          const Icon(
+                            FontAwesomeIcons.locationDot,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: mediaQuery.size.width * 0.015),
+                        ],
+                      ),
+                      buttonColour: kPrimaryAccentColour,
+                      buttonFontSize: mediaQuery.size.width * 0.035,
+                      onPressed: () => Get.to(
+                          () => const SearchLocationPage(isSearchFeed: false)),
+                      buttonHeight: mediaQuery.size.width * 0.10,
+                      buttonLength: mediaQuery.size.width * 0.40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      );
+    }
 
     Widget googleMap() {
       return Obx(() => locController.latitude.value != null &&
