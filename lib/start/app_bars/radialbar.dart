3ac424416be8dc28496f5ec2d02f640176a2094d
Julia Pak <jpak0444@gmail.com>
2022-11-17 21:03:45 -0500

    comment

--- lib/start/app_bars/radialbar.dart
+++ lib/start/app_bars/radialbar.dart
@@ -26,7 +26,7 @@ AppBar myRadialBar(BuildContext context) {
           //bag icon
           icon: Icon(Icons.shopping_bag_rounded),
           onPressed: () {
-            //do something here
+            //do something here. get.to the cart page!
           },
         ),
       )
