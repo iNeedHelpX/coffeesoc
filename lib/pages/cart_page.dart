97833d16b4d4f509454e00b7a11c695c2636316d
Julia Pak <jpak0444@gmail.com>
2022-11-18 18:38:47 -0500

    cart button

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:coffeesoc/colors/colours_list.dart';
 import 'package:coffeesoc/pages/widgets/custombackbutton.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -13,7 +14,24 @@ class CartPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
           child: Stack(
-            children: [pinkBackButton()],
+            children: [
+              InkWell(
+                // Splash color
+                splashColor: lightgoldbg,
+                onTap: () {
+                  //go back to the home page
+                  Get.back();
+                  //Navigator.pop(context);
+                },
+
+                child: Icon(
+                  Icons.chevron_left_rounded,
+                  color: greytext,
+                  size: 36,
+                ),
+              ),
+              // pinkBackButton()
+            ],
           ),
         ),
       ),
