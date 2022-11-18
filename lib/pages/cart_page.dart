fbde8a50bf0caabf18781c345443a137a8190b6f
Julia Pak <jpak0444@gmail.com>
2022-11-17 23:29:43 -0500

    cart page colorsw

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,13 +1,32 @@
 import 'package:coffeesoc/colors/colours_list.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: barback,
+    return Material(
+      child: Container(
+        color: barback,
+        child: Stack(children: [
+          SafeArea(
+            //back button
+            child: IconButton(
+              onPressed: () {
+                //back to home
+                Get.back();
+                //Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.backspace,
+                color: Color.fromARGB(255, 247, 0, 97),
+              ),
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
