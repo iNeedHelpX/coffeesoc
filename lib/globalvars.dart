a80b16c8765ef5813b81317b2b352c8418fd9691
Julia Pak <jpak0444@gmail.com>
2022-11-19 14:02:51 -0500

    configuration

--- lib/globalvars.dart
+++ lib/globalvars.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:coffeesoc/config/config.dart';
 import 'package:coffeesoc/controllers/location_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -15,3 +16,4 @@ LoginController loginController = LoginController.instance;
 // LocationController locationController = LocationController.instance;
 //other variables
 Logger logger = Logger();
+var rapydConfig = Configurations();
