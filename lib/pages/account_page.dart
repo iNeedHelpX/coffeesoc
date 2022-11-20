64f609f77768b66f056acadfafd10048f7675a33
Julia Pak <jpak0444@gmail.com>
2022-11-20 06:13:11 -0500

    logout color

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -26,7 +26,7 @@ class _AccountPageState extends State<AccountPage> {
                   ),
                   CustomButton(
                       text: "Logout",
-                      bgColor: medpink,
+                      bgColor: prettyPurple,
                       onTap: () {
                         loginController.signOut();
                       }),
