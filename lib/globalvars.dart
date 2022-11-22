d4d3f6ca347101f9c97f3d3461d1982f1232561d
Julia Pak <jpak0444@gmail.com>
2022-11-22 01:34:22 -0500

    ok

--- lib/globalvars.dart
+++ lib/globalvars.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:coffeesoc/config/config.dart';
-import 'package:coffeesoc/controllers/location_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:coffeesoc/controllers/login_controller.dart';
