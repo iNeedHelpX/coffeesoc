0bbbbb4f9c3edefd9680040ba50a6bd98b058709
Julia Pak <jpak0444@gmail.com>
2022-12-02 18:30:18 -0500

    rec icn

--- lib/start/app_bars/listbaricons.dart
+++ lib/start/app_bars/listbaricons.dart
@@ -1,4 +1,4 @@
-import 'package:coffeesoc/icons/panda_icon.dart';
+import 'package:coffeesoc/icons/rec_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:coffeesoc/icons/account_icon.dart';
 import 'package:coffeesoc/icons/cup_icon.dart';
