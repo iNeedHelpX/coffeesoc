e59ef92d4aab8a06fe5f369ecd52d974a6bec640
Julia Pak <jpak0444@gmail.com>
2022-12-04 09:22:18 -0500

    ok

--- lib/pages/Sub_pages/details_pg.dart
+++ lib/pages/Sub_pages/details_pg.dart
@@ -1,4 +1,5 @@
 import 'package:coffeesoc/colors/colours_list.dart';
+import 'package:coffeesoc/pages/widgets/custombackbutton.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,12 +7,12 @@ class DetailsView extends StatelessWidget {
   //the return types required when you call detailsview
   final String name;
   final String imgUrl;
+
   const DetailsView({super.key, required this.imgUrl, required this.name});
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -35,22 +36,34 @@ class DetailsView extends StatelessWidget {
                 //put things here?
               ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 15, top: 40),
-              child: FloatingActionButton(
-                mini: true,
-                foregroundColor: lightgoldbg,
-                hoverColor: containerback,
-                backgroundColor: tabicon.withOpacity(0.8),
-                onPressed: () {
-                  Get.back();
-                },
-                child: Icon(Icons.backspace),
-              ),
-            ),
+            backButton(),
           ],
         ),
       ),
     );
+  }
+
+  Padding backButton() {
+    return Padding(
+        padding: const EdgeInsets.only(left: 15, top: 40),
+        child: ClipOval(
+          child: Material(
+            color: prettyPurple, // Button color
+            child: InkWell(
+              splashColor: tabicon, // Splash color
+              onTap: () {
+                Get.back();
+              },
+              child: SizedBox(
+                width: 40,
+                height: 40,
+                child: Icon(
+                  Icons.chevron_left_outlined,
+                  color: greytext2,
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
