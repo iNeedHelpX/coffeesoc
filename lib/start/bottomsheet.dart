1f1527d7ca7ac1789b2597799cedf943d2ae00fa
Julia Pak <jpak0444@gmail.com>
2022-12-12 17:39:47 -0500

    replace

--- lib/start/bottomsheet.dart
+++ lib/start/bottomsheet.dart
@@ -1,9 +1,12 @@
 import 'package:coffeesoc/colors/colours_list.dart';
+import 'package:coffeesoc/globalvars.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 Future<dynamic> bottomSheet(BuildContext context) async {
+  User? user;
   return showModalBottomSheet(
       backgroundColor: prettyPurple,
       shape: RoundedRectangleBorder(
@@ -20,7 +23,7 @@ Future<dynamic> bottomSheet(BuildContext context) async {
                 height: 20,
               ),
               Text(
-                "Replace asap Julia",
+                "Replace ASAP ${auth.currentUser!.displayName}",
                 style: GoogleFonts.courgette(
                     fontSize: 24,
                     fontWeight: FontWeight.w600,
