24fb0744ede8b7cc978f7dac53327804d889a0c4
Julia Pak <jpak0444@gmail.com>
2022-12-13 23:29:30 -0500

    kk

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -37,10 +37,7 @@ class _AccountPageState extends State<AccountPage> {
                     titleText: 'Coming soon Pay with QR code',
                     subText: 'coming soon',
                   ),
-                  SettingsCard(
-                    titleText: 'Coming soon Pay with QR code',
-                    subText: 'coming soon',
-                  ),
+
                   SizedBox(
                     height: 40,
                   ),
