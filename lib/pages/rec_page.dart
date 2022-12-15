d7e94764ffd72734e2ec4a64faba13ff1a35d46e
Julia Pak <jpak0444@gmail.com>
2022-12-15 03:51:33 -0500

    reccomended

--- lib/pages/rec_page.dart
+++ lib/pages/rec_page.dart
@@ -1,5 +1,7 @@
+import 'package:coffeesoc/globalvars.dart';
 import 'package:coffeesoc/pages/widgets/neurobox.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PandaPage extends StatelessWidget {
   const PandaPage({super.key});
@@ -10,9 +12,28 @@ class PandaPage extends StatelessWidget {
       child: SafeArea(
         child: Stack(
           children: [
-            NeuromorphicBox(
-                boomtitle: "Recommended by others",
-                subtext: "shops visited by other people in your network"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    "Hello ${auth.currentUser!.displayName}",
+                    style: GoogleFonts.cabin(
+                        fontSize: 22,
+                        color: Color.fromARGB(255, 253, 1, 152),
+                        fontWeight: FontWeight.w800),
+                  ),
+                ),
+                NeuromorphicBox(
+                    boomtitle: "Recommended by others",
+                    subtext:
+                        "${auth.currentUser!.displayName}, check out shops visited by other people in your network"),
+              ],
+            ),
           ],
         ),
       ),
