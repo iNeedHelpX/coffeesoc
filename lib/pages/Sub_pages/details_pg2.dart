dcc18cda26eab22c73e25920d900eb3df06be8ab
Julia Pak <jpak0444@gmail.com>
2022-12-18 11:58:58 -0500

    text

--- lib/pages/Sub_pages/details_pg2.dart
+++ lib/pages/Sub_pages/details_pg2.dart
@@ -1,6 +1,8 @@
 import 'package:coffeesoc/colors/colours_list.dart';
 import 'package:coffeesoc/pages/widgets/back_button.dart';
+import 'package:coffeesoc/pages/widgets/icons_textwidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gradients_reborn/flutter_gradients_reborn.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class DetailsPage2 extends StatelessWidget {
@@ -17,7 +19,6 @@ class DetailsPage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     final MediaQueryData mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      backgroundColor: prettyPurple,
       body: Stack(
         children: [
           Positioned(
@@ -44,12 +45,16 @@ class DetailsPage2 extends StatelessWidget {
           Positioned(
             left: 0,
             right: 0,
-            top: mediaQuery.size.height / 2.6,
+            bottom: 0,
+            top: mediaQuery.size.height / 2.7,
             child: Container(
-              padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+              padding: EdgeInsets.only(left: 15, right: 15, top: 10),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(25),
+                  topLeft: Radius.circular(25),
+                ),
+                gradient: FlutterGradients.kindSteel(tileMode: TileMode.clamp),
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -62,14 +67,76 @@ class DetailsPage2 extends StatelessWidget {
                         fontWeight: FontWeight.w700),
                   ),
                   Text(
+                    "Seating: 20",
+                    style: GoogleFonts.karla(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w900,
+                      color: Color.fromARGB(255, 248, 2, 145),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star,
+                            color: Colors.red,
+                            size: 15,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconAndText(
+                          icn: Icons.reviews_sharp,
+                          text: "Reviews",
+                          color: textblue2,
+                          icnColor: orangetext),
+                      IconAndText(
+                          icn: Icons.favorite,
+                          text: "Add to favorite",
+                          color: Colors.black,
+                          icnColor: Color.fromARGB(255, 252, 6, 92)),
+                      IconAndText(
+                          icn: Icons.pin_drop,
+                          text: "GoogleMaps",
+                          color: tabicon,
+                          icnColor: Color.fromARGB(255, 11, 144, 49)),
+                    ],
+                  ),
+                  Text(
                     address,
                     style: GoogleFonts.karla(
                       fontSize: 16,
                       fontWeight: FontWeight.w500,
-                      color: Color.fromARGB(255, 252, 0, 105),
+                      color: barback,
                     ),
                   ),
+                  SizedBox(
+                    height: 40,
+                  ),
                 ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
+              decoration: BoxDecoration(
+                gradient:
+                    FlutterGradients.sleeplessNight(tileMode: TileMode.clamp),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
               ),
             ),
           )
---
+++ lib/pages/widgets/icons_textwidget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class IconAndText extends StatelessWidget {
+  final IconData icn;
+  final String text;
+  final Color color;
+  final Color icnColor;
+  const IconAndText(
+      {super.key,
+      required this.icn,
+      required this.text,
+      required this.color,
+      required this.icnColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icn,
+          color: icnColor,
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Text(
+          text,
+          textAlign: TextAlign.left,
+          style: GoogleFonts.karla(fontSize: 14, color: color),
+        ),
+      ],
+    );
+  }
+}
